fa7641ffca659c3c6d3f61eaf1626f1625f79296
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-11-07 22:35:16 -0300

    Implementing voice recognition

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,7 @@
+package io.ionic.starter;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.speechrecognition.SpeechRecognition;
+
+public class MainActivity extends BridgeActivity {
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpeechRecognition } from '@capacitor-community/speech-recognition';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  listening = false;
+  languages: string[];
+  language = 'en-US';
+  translateText: string;
+
+  constructor() {
+    SpeechRecognition.requestPermission();
+    this.getLanguages();
+  }
+
+  async getLanguages() {
+    await SpeechRecognition.getSupportedLanguages().then(data => this.languages = data.languages);
+  }
+
+  changeLanguage(event: any) {
+    this.language = event.detail.value;
+  }
+
+  async startListening() {
+    const { available } = await SpeechRecognition.available();
+
+    if (available) {
+      this.listening = true;
+      SpeechRecognition.start({
+        language: this.language,
+        maxResults: 1,
+        partialResults: false,
+        popup: false,
+    }).then(result => {
+        if (result.matches[0]) {
+          this.translateText = result.matches[0];
+          this.stopListening();
+        }
+    });
+    }
+
+  }
+
+  stopListening() {
+    this.listening = false;
+    SpeechRecognition.stop();
+  }
 
 }
